9e59e14ade111814281d0e180cc5e2c78635defc
akiyama <daniilsav2000@yandex.ru>
2025-02-17 01:51:11 +0400

    Fix: sidebar can open and overflow behavior updated

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,24 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import { Text } from 'src/ui/text';
 
 export const ArticleParamsForm = () => {
+	const [panelState, setPanelState] = useState<boolean>(false);
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={panelState}
+				onClick={() => setPanelState(!panelState)}
+			/>
+			<aside
+				className={clsx(styles.container, panelState && styles.container_open)}>
 				<form className={styles.form}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
